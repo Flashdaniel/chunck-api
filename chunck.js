34871b387d335db7e278e6fc32d80e7dcd8adb6e
NWeze Daniel <flashdaniel0@gmail.com>
2024-06-15 14:33:02 +0100

    modified chunck.js and Add .gitignore and README.md files

--- chunck.js
+++ chunck.js
@@ -1,1 +1,16 @@
-function chunck(array, length) {}
+function chunck(array, length) {
+  let result = [];
+  let length = array.length / length;
+  length = Number.isInteger(length) ? length : Math.floor(++length);
+
+  const newArray = array.slice();
+  const firstIndex = 0;
+  const lastIndex = length;
+
+  while (newArray.length) {
+    result.push(newArray.slice(firstIndex, lastIndex));
+    newArray = newArray.toSpliced(firstIndex, lastIndex);
+  }
+
+  return result;
+}
